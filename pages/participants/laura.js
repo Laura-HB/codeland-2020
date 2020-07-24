33773fe1aab1ce78c0d7f86fc6cdd941c7870767
Laura Brown <48622735+Laura-HB@users.noreply.github.com>
2020-07-24 10:53:39 -0400

    Create laura.js

---
+++ pages/participants/laura.js
@@ -0,0 +1,16 @@
+import Layout from "../../components/Layout";
+import About from "../../components/About";
+
+function Laura() {
+  return (
+    <Layout>
+      <About
+        name="Laura Brown"
+        imageUrl="https://images.unsplash.com/photo-1519052537078-e6302a4968d4"
+        description="Hey, there! My name is Laura, and I'm new to coding! I love reading and being outside, preferably at the same time."
+      />
+    </Layout>
+  );
+}
+
+export default Laura;
